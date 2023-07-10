e6e8ef482d618c4b4efb74b94fc5b890af3006e1
Filip Chrzuszcz <42840114+arctickey@users.noreply.github.com>
2023-07-10 09:28:56 +0200

    Update plots.py

--- src/plots.py
+++ src/plots.py
@@ -5,23 +5,16 @@ import pandas as pd
 import ast
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 
 # %%
 dataset = Dataset("/workspaces/thesis/wiki-RfA.txt")
 dataframe = dataset.read_file()
 df_preprocessed = dataset.preprocess_dataframe(df=dataframe)
-votes_coutns = df_preprocessed["VOTE"].value_counts().reset_index()
-votes_coutns["VOTE"] = votes_coutns["VOTE"].astype(pd.Categorical)
-fig_votes = px.bar(
-    df_preprocessed["VOTE"].value_counts().reset_index(),
-    x="VOTE",
-    y="count",
-    title="Vote distribution",
-)
-fig_votes.update_layout(showlegend=False, xaxis_type="category")
+
 # %%
 
-results = pd.read_csv("/workspaces/thesis/src/grid_search_results.csv")
+results = pd.read_csv("/workspaces/thesis/grid_search_results.csv")
 metrics = ["precision", "recall"]
 results["recall"] = (
     results["recall"]
@@ -52,56 +45,93 @@ best.columns = ["SGCN", "LGBM"]
 best["SGCN"] = best["SGCN"].astype(float)
 best["LGBM"] = best["LGBM"].astype(float)
 # %%
-fig = px.bar(best.loc[best.index.isin(["Accuracy", "ROC"]), :], barmode="group", orientation="h")
+plt.style.use("fast")
+
+
+xd = best.loc[best.index.isin(["Accuracy", "ROC"]), :]
+xd.columns.name = "Value"
+xd.index.name = "Metric"
+xd.index = ["Accuracy", "AUC"]
+xd.plot.barh(title="Accuracy and AUC of best models").legend(
+    loc="center left", bbox_to_anchor=(1, 0.5), reverse=True
+)
+plt.savefig("/workspaces/thesis/src/plots/acc_roc1.png", dpi=300, bbox_inches="tight")
+# %%
+fig = px.bar(xd, barmode="group", orientation="h")
 fig.update_layout(
-    title="Accuracy and ROC of best chosen models",
-    font=dict(size=25),
+    title=dict(text="Accuracy and AUC of best models", y=0.99),
+    font=dict(size=40),
     xaxis_title="Value",
-    yaxis_title="Method",
+    yaxis_title="Metric",
     legend=dict(title="Model", traceorder="reversed"),
     template="simple_white",
 )
-fig.write_image("/workspaces/thesis/src/plots/acc_roc.png", width=1000, height=600)
+fig.write_image("/workspaces/thesis/src/plots/acc_roc.png", width=1500, height=1000)
 fig.show()
 
 # %%
 xd = best.loc[
-    best.index.isin(["precision_negative", "precision_nonexistent", "precision_positive"]), :
+    best.index.isin(
+        ["precision_negative", "precision_nonexistent", "precision_positive"]
+    ),
+    :,
 ]
-xd.index = [x.capitalize() for x in xd.index]
+xd.index = [x[len("precision_") :].capitalize() for x in xd.index]  # noqa
+
+xd.columns.name = "Value"
+xd.index.name = "Class type"
+xd.plot.barh(title="Precision of best models").legend(
+    loc="center left", bbox_to_anchor=(1, 0.5), reverse=True
+)
+plt.savefig("/workspaces/thesis/src/plots/precision1.png", dpi=300, bbox_inches="tight")
+
+# %%
 fig_prec = px.bar(
     xd,
     barmode="group",
     orientation="h",
 )
 fig_prec.update_layout(
-    title="Precision of best chosen models",
-    font=dict(size=25),
+    title=dict(text="Precision of best models", y=0.99),
+    font=dict(size=40),
     xaxis_title="Value",
-    yaxis_title="Method",
+    yaxis_title="Class name",
     legend=dict(title="Model", traceorder="reversed"),
     template="simple_white",
 )
-fig_prec.write_image("/workspaces/thesis/src/plots/precision.png", width=1000, height=600)
+fig_prec.write_image(
+    "/workspaces/thesis/src/plots/precision.png", width=1500, height=1000
+)
 fig_prec.show()
 
 # %%
-xd = best.loc[best.index.isin(["recall_negative", "recall_nonexistent", "recall_positive"]), :]
-xd.index = [x.capitalize() for x in xd.index]
+xd = best.loc[
+    best.index.isin(["recall_negative", "recall_nonexistent", "recall_positive"]), :
+]
+xd.index = [x[len("recall_") :].capitalize() for x in xd.index]  # noqa
+
+xd.columns.name = "Value"
+xd.index.name = "Class type"
+xd.plot.barh(title="Recall of best models").legend(
+    loc="center left", bbox_to_anchor=(1, 0.5), reverse=True
+)
+plt.savefig("/workspaces/thesis/src/plots/recall1.png", dpi=300, bbox_inches="tight")
+
+# %%
 fig_rec = px.bar(
     xd,
     barmode="group",
     orientation="h",
 )
 fig_rec.update_layout(
-    title="Recall of best chosen models",
-    font=dict(size=25),
+    title=dict(text="Recall of best models", y=0.99),
+    font=dict(size=40),
     xaxis_title="Value",
-    yaxis_title="Method",
+    yaxis_title="Class name",
     legend=dict(title="Model", traceorder="reversed"),
     template="simple_white",
 )
-fig_rec.write_image("/workspaces/thesis/src/plots/recall.png", width=1000, height=600)
+fig_rec.write_image("/workspaces/thesis/src/plots/recall.png", width=1500, height=1000)
 fig_rec.show()
 # %%
 
@@ -113,5 +143,62 @@ sub = nx.subgraph(graph, list(graph.nodes())[:50])
 sub_edges = list(sub.edges(data=True))
 colors = ["r" if sub_edges[i][2]["VOTE"] == -1 else "g" for i in range(len(sub_edges))]
 nx.draw_networkx(sub, arrows=True, edge_color=colors, with_labels=False)
-plt.savefig("/workspaces/thesis/src/plots/network.png", dpi=300)
+plt.savefig("/workspaces/thesis/src/plots/network.png", dpi=300, bbox_inches="tight")
+
+
 # %%
+def perm_importance(model, X_train, X_test, y_test):
+    from sklearn.inspection import permutation_importance
+
+    r = permutation_importance(model, X_test, y_test)
+    feat_importances = pd.Series(r["importances_mean"], index=X_train.columns)
+    feat_importances = feat_importances.reset_index()
+    feat_importances.columns = ["Predictor", "Score"]
+    feat_importances.sort_values(by="Score", inplace=True, ascending=False)
+    feat_importances["Score"] = np.round(feat_importances["Score"], 3)
+    feat_importances["Predictor"] = (
+        feat_importances["Predictor"]
+        .replace(
+            {
+                "_2": "_target",
+                "_1": "_source",
+                "SOURCE": "source_name",
+                "TARGET": "target_name",
+            },
+            regex=True,
+        )
+        .replace({"adj_matrix_target": "adj_matrix_2"}, regex=True)
+    )
+    feat_importances.to_latex(
+        index=False,
+        caption="Permutation feature importance from the model",
+        label="Feature importance",
+    )
+
+
+def roc_curve_class(y_train, y_test, proba, model_name):
+    from sklearn.metrics import RocCurveDisplay
+    from itertools import cycle
+    import matplotlib.pyplot as plt
+    from sklearn.preprocessing import LabelBinarizer
+
+    label_binarizer = LabelBinarizer().fit(y_train)
+    y_onehot_test = label_binarizer.transform(y_test)
+    plt.style.use("fast")
+    fig, ax = plt.subplots(figsize=(6, 6))
+    colors = cycle(["aqua", "darkorange", "cornflowerblue"])
+    target_names = ["negative", "nonexistent", "positive"]
+    for class_id, color in zip(range(3), colors):
+        RocCurveDisplay.from_predictions(
+            y_onehot_test[:, class_id],
+            proba[:, class_id],
+            name=f"ROC curve for {target_names[class_id]}",
+            color=color,
+            ax=ax,
+        )
+    plt.axis("square")
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title(f"ROC curves for the {model_name} model")
+    plt.legend()
+    plt.savefig(f"roc_{model_name}.png", dpi=300, bbox_inches="tight")
